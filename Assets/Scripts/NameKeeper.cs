389f2fa2c5f5a56d0cb847ff07b5308ddb298b6f
Praveen Lockuliyanage <s115403@ltisdschools.net>
2026-04-24 12:05:24 -0500

    FINISHED MUSIC AND USERNAMES

--- Assets/Scripts/NameKeeper.cs
+++ Assets/Scripts/NameKeeper.cs
@@ -1,18 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro; // Required for TextMeshPro
 
-public class NameKeeper : MonoBehaviour
+public class UsernameHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField inputField;
+    public string playerUsername;
+
+    public void SaveName()
     {
-        
+        // Capture text from the UI
+        playerUsername = inputField.text;
+
+        // Save locally so it persists after closing the game
+        PlayerPrefs.SetString("SavedUsername", playerUsername);
+        PlayerPrefs.Save();
+
+        Debug.Log("Username saved: " + playerUsername);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        // Load the name if it was previously saved
+        if (PlayerPrefs.HasKey("SavedUsername"))
+        {
+            playerUsername = PlayerPrefs.GetString("SavedUsername");
+            inputField.text = playerUsername;
+        }
     }
 }
