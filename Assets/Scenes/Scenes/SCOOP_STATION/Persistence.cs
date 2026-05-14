ee1da7b416ccaa33b91a4c525e82f24dd73333ac
Vaishnavi Senthil <vaisen2626@gmail.com>
2026-05-14 11:17:37 -0500

    scene manage

--- Assets/Scenes/Scenes/SCOOP_STATION/Persistence.cs
+++ Assets/Scenes/Scenes/SCOOP_STATION/Persistence.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 public class PersistBetweenScenes : MonoBehaviour
 {
     public GameObject obj1;
+   
     private void Awake()
     {
         DontDestroyOnLoad(obj1);
---
+++ Assets/Scenes/Scenes/SCOOP_STATION/sceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+public class sceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
